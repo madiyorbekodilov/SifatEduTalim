2bf8ffd1484de0c9674d57abf2c5f505accc7b9a
Ferganskiy <odilovmadiyor029@gmail.com>
2024-02-21 19:51:52 +0500

    Participate entity added

---
+++ SifatEdu.Domain/Entities/Participate.cs
@@ -0,0 +1,12 @@
+﻿using SifatEdu.Domain.Commons;
+
+namespace SifatEdu.Domain.Entities;
+
+public class Participate : Auditable
+{
+    public long TesId { get; set; }
+    public long CountOfTrue { get; set; }
+
+    public long UserId { get; set; }
+    public User User { get; set; }
+}
