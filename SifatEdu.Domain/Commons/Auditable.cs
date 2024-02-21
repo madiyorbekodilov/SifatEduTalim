9b05382cc12922842b5ec3045f965d8de6fe99c0
Ferganskiy <odilovmadiyor029@gmail.com>
2024-02-21 09:06:29 +0500

    Auditable added

---
+++ SifatEdu.Domain/Commons/Auditable.cs
@@ -0,0 +1,9 @@
+﻿namespace SifatEdu.Domain.Commons;
+
+public abstract class Auditable
+{
+    public long Id { get; set; }
+    public DateTime CreateAt { get; set; }
+    public DateTime UpdateAt { get; set; }
+    public bool IsDeleted { get; set; }
+}
