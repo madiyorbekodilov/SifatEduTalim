6016712edc7fdc7fb81285ca5cbe6b4f41bfe72b
Ferganskiy <odilovmadiyor029@gmail.com>
2024-02-21 20:35:09 +0500

    Repasitory's Interface added

---
+++ SifatEdu.Data/IRepasitories/IRepasitory.cs
@@ -0,0 +1,15 @@
+﻿using SifatEdu.Domain.Commons;
+using System.Linq.Expressions;
+
+namespace SifatEdu.Data.IRepasitories;
+
+public interface IRepasitory<T> where T : Auditable
+{
+    Task CreateAsync(T entity);
+    void Update(T entity);
+    void Delete(T entity);
+    void Destroy(T entity);
+    ValueTask<T> SelectAsync(Expression<Func<T, bool>> expression, string[] includes = null);
+    IQueryable<T> SelectAll(Expression<Func<T, bool>> expression = null, bool isNoTracked = true, string[] includes = null);
+    ValueTask SaveAsync();
+}
