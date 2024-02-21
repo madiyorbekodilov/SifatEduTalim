afcfbaef9cd43c77d12ece3df7a9a3701c8f1196
Ferganskiy <odilovmadiyor029@gmail.com>
2024-02-21 15:18:14 +0500

    Attachment entity added

---
+++ SifatEdu.Domain/Entities/Attachment.cs
@@ -0,0 +1,9 @@
+﻿using SifatEdu.Domain.Commons;
+
+namespace SifatEdu.Domain.Entities;
+
+public class Attachment : Auditable
+{
+    public string FilePath { get; set; }
+    public string FileName { get; set; }
+}
