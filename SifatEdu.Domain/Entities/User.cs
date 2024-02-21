52104c2b469896f76835ce0c2af53eae1c0640d9
Ferganskiy <odilovmadiyor029@gmail.com>
2024-02-21 09:18:49 +0500

    User Entity added

---
+++ SifatEdu.Domain/Entities/User.cs
@@ -0,0 +1,16 @@
+﻿using SifatEdu.Domain.Commons;
+using SifatEdu.Domain.Enums;
+
+namespace SifatEdu.Domain.Entities;
+
+public class User : Auditable
+{
+    public string FirsName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+    public string Phone { get; set; }
+    public UserRole Role { get; set; }
+    public DateTime DateOfBirth { get; set; }
+
+}
