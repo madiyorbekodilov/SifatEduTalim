08381d2632782c178522c4dd509002337776a7ab
Ferganskiy <odilovmadiyor029@gmail.com>
2024-02-21 20:01:06 +0500

    bugs fixed

--- SifatEdu.Domain/Commons/Auditable.cs
+++ SifatEdu.Domain/Commons/Auditable.cs
@@ -3,7 +3,7 @@
 public abstract class Auditable
 {
     public long Id { get; set; }
-    public DateTime CreateAt { get; set; }
-    public DateTime UpdateAt { get; set; }
+    public DateTime CreateAt { get; set; } = DateTime.UtcNow;
+    public DateTime? UpdateAt { get; set; }
     public bool IsDeleted { get; set; }
 }
