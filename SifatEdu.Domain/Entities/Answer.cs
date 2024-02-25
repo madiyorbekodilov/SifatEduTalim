d8f35063b15e1394897b2f7ba1826c7134db6b9e
Ferganskiy <odilovmadiyor029@gmail.com>
2024-02-25 16:42:43 +0500

    bugs fixed

--- SifatEdu.Domain/Entities/Answer.cs
+++ SifatEdu.Domain/Entities/Answer.cs
@@ -4,6 +4,9 @@ namespace SifatEdu.Domain.Entities;
 
 public class Answer : Auditable
 {
+    public long QuestionId { get; set; }
+    public Question Question { get; set; }
+
     public string Javob { get; set; }
     public bool IsTrue { get; set; }
 }
