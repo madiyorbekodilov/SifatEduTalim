3877001dd79160658ce738a3124888f6d21dc186
Ferganskiy <odilovmadiyor029@gmail.com>
2024-02-26 18:09:59 +0500

    User's Dto added

---
+++ SifatEdu.Service/DTOs/User/UserCreationDto.cs
@@ -0,0 +1,14 @@
+﻿using SifatEdu.Domain.Enums;
+
+namespace SifatEdu.Service.DTOs.User;
+
+public class UserCreationDto
+{
+    public string FirsName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+    public string Phone { get; set; }
+    public UserRole Role { get; set; }
+    public DateTime DateOfBirth { get; set; }
+}
---
+++ SifatEdu.Service/DTOs/User/UserUpdateDto.cs
@@ -0,0 +1,13 @@
+﻿using SifatEdu.Domain.Enums;
+
+namespace SifatEdu.Service.DTOs.User;
+
+public class UserUpdateDto
+{
+    public long Id { get; set; }
+    public string FirsName { get; set; }
+    public string LastName { get; set; }
+    public string Phone { get; set; }
+    public UserRole Role { get; set; }
+    public DateTime DateOfBirth { get; set; }
+}
