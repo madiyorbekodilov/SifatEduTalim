227bb04dc89d7405dbf440843377c49a9e3c1402
Ferganskiy <odilovmadiyor029@gmail.com>
2024-02-26 18:27:20 +0500

    Attachment's dto added

---
+++ SifatEdu.Service/DTOs/Attachment/AttachmentCreationDto.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace SifatEdu.Service.DTOs.Attachment;
+
+public class AttachmentCreationDto
+{
+    public IFormFile FormFile { get; set; }
+}
---
+++ SifatEdu.Service/DTOs/Attachment/AttachmentResultDto.cs
@@ -0,0 +1,8 @@
+﻿namespace SifatEdu.Service.DTOs.Attachment;
+
+public class AttachmentResultDto
+{
+    public long Id { get; set; }
+    public string FileName { get; set; }
+    public string FilePath { get; set; }
+}
