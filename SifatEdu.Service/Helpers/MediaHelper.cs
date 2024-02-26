41b4b028b72ba18748fa5b6164fe29d0de473ebd
Ferganskiy <odilovmadiyor029@gmail.com>
2024-02-26 16:01:38 +0500

    MediaHelper added

---
+++ SifatEdu.Service/Helpers/MediaHelper.cs
@@ -0,0 +1,27 @@
+﻿namespace SifatEdu.Service.Helpers;
+
+public class MediaHelper
+{
+    public static string MakeImageName(string filename)
+    {
+        FileInfo fileInfo = new FileInfo(filename);
+        string extention = fileInfo.Extension;
+        string name = "IMG_" + Guid.NewGuid() + extention;
+        return name;
+    }
+
+    public static string[] GetImageExtention()
+    {
+        return new string[]
+        {
+            // JPG files
+            ".jpg", ".jpeg",
+            // Png files
+            ".png",
+            // Bmp files
+            ".bmp",
+            // Svg files
+            ".svg"
+        };
+    }
+}
