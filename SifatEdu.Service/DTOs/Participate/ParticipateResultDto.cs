d22251998f46cc5225ca05ab7eda2ad956dc2ad0
Ferganskiy <odilovmadiyor029@gmail.com>
2024-02-26 20:16:54 +0500

    bugs fixed

--- SifatEdu.Service/DTOs/Participate/ParticipateResultDto.cs
+++ SifatEdu.Service/DTOs/Participate/ParticipateResultDto.cs
@@ -1,4 +1,6 @@
 ﻿using SifatEdu.Domain.Entities;
+using SifatEdu.Service.DTOs.Test;
+using SifatEdu.Service.DTOs.User;
 
 namespace SifatEdu.Service.DTOs.Participate;
 
@@ -7,4 +9,7 @@ public class ParticipateResultDto
     public long Id { get; set; }
     public long CountOfTrue { get; set; }
     public long Ball { get; set; }
+
+    public TestResultDto TestResult { get; set; }
+    public UserResultDto UserResult { get; set; }
 }
