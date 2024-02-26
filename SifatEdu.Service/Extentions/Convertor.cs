ab35e11d62dd9d14f5b6b435c040d4881bc846f2
Ferganskiy <odilovmadiyor029@gmail.com>
2024-02-26 11:44:11 +0500

    Convertor added

---
+++ SifatEdu.Service/Extentions/Convertor.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace SifatEdu.Service.Extentions;
+
+public static class Convertor
+{
+    public static byte[] ToByte(this IFormFile formFile)
+    {
+        using var memoryStream = new MemoryStream();
+        formFile.CopyTo(memoryStream);
+        return memoryStream.ToArray();
+    }
+}
