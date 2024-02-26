3ffdf4e24a01cadc8c75aa8ea13facc839a8d28d
Ferganskiy <odilovmadiyor029@gmail.com>
2024-02-26 11:37:00 +0500

    CustomException added

---
+++ SifatEdu.Service/Exceptions/CustomException.cs
@@ -0,0 +1,11 @@
+﻿namespace SifatEdu.Service.Exceptions;
+
+public class CustomException : Exception
+{
+	public CustomException(int statusCode, string message) : base(message)
+	{
+		this.statusCode = statusCode;
+	}
+
+	public int statusCode { get; set; } 
+}
