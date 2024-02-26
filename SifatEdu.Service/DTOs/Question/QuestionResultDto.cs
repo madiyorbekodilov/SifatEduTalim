64dc77d21437b61650274d51ba15317b5734227e
Ferganskiy <odilovmadiyor029@gmail.com>
2024-02-26 18:28:36 +0500

    bugs fixed

--- SifatEdu.Service/DTOs/Question/QuestionResultDto.cs
+++ SifatEdu.Service/DTOs/Question/QuestionResultDto.cs
@@ -1,8 +1,12 @@
-﻿namespace SifatEdu.Service.DTOs.Question;
+﻿using SifatEdu.Service.DTOs.Attachment;
+
+namespace SifatEdu.Service.DTOs.Question;
 
 public class QuestionResultDto
 {
     public long Id { get; set; }
     public string Savol { get; set; }
     public bool isCode { get; set; }
+
+    public AttachmentResultDto Attachment { get; set; }
 }
