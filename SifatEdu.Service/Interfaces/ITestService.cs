1e5ef6dd81f090a38ccd2832c67be1e8cbc66046
Ferganskiy <odilovmadiyor029@gmail.com>
2024-02-29 11:36:19 +0500

    Test Service's Interface added

---
+++ SifatEdu.Service/Interfaces/ITestService.cs
@@ -0,0 +1,19 @@
+﻿using SifatEdu.Domain.Entities;
+using SifatEdu.Service.DTOs.Test;
+
+namespace SifatEdu.Service.Interfaces;
+
+public interface ITestService
+{
+    Task<bool> DeleteAsync(long id);
+    Task<bool> SearchExamAsync(long id);
+    Task<IEnumerable<Test>> GetAllAsync();
+    Task<TestResultDto> GetByIdAsync(long id);
+    Task<IEnumerable<TestResultDto>> EndedExams();
+    Task<IEnumerable<TestResultDto>> FutureExams();
+    Task<IEnumerable<TestResultDto>> CurrentExams();
+    Task<TestResultDto> ModifyAsync(TestUpdateDto examUpdate);
+    Task<IEnumerable<TestResultDto>> GetByTitleAsync(string title);
+    Task<TestResultDto> CreateAsync(TestCreationDto examCreation);
+    Task<IEnumerable<TestResultDto>> GetByNearExamAsync(DateTime dateTime);
+}
