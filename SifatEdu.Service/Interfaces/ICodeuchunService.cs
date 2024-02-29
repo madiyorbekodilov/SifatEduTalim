21cd5966558bfedafd8e7445c7510c9f2a7e5a00
Ferganskiy <odilovmadiyor029@gmail.com>
2024-02-29 11:57:32 +0500

    ICodeuchunService

---
+++ SifatEdu.Service/Interfaces/ICodeuchunService.cs
@@ -0,0 +1,13 @@
+﻿using SifatEdu.Domain.Entities;
+using SifatEdu.Service.DTOs.Answer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SifatEdu.Service.Interfaces;
+
+public interface ICodeuchunService
+{
+}
