f98eba52515630656b56a0ab854d5b43d8dc012a
Ferganskiy <odilovmadiyor029@gmail.com>
2024-02-29 11:33:57 +0500

    Question service's interface added

---
+++ SifatEdu.Service/Interfaces/IQuestionService.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using SifatEdu.Service.DTOs.Question;
+
+namespace SifatEdu.Service.Interfaces;
+
+public interface IQuestionService
+{
+    Task<bool> DeleteAsync(long id);
+    Task<QuestionResultDto> GetByIdAsync(long id);
+    Task<IEnumerable<QuestionResultDto>> GetAllAsync();
+    Task<bool> ModifyImageAsync(long id, IFormFile image);
+    Task<QuestionResultDto> ModifyAsync(QuestionUpdateDto dto);
+    Task<QuestionResultDto> CreateAsync(QuestionCreationDto dto);
+    Task<IEnumerable<QuestionResultDto>> GetByUserIdAsync(long userId);
+    Task<IEnumerable<QuestionResultDto>> SearchAsync(string searchTerm);
+}
