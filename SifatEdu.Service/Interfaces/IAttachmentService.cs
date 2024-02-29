11476f9c36fad5eff9f0aa99ddb6db4b0ee4e22d
Ferganskiy <odilovmadiyor029@gmail.com>
2024-02-29 11:23:46 +0500

    Attachment Service's Interface added

---
+++ SifatEdu.Service/Interfaces/IAttachmentService.cs
@@ -0,0 +1,10 @@
+﻿using SifatEdu.Domain.Entities;
+using SifatEdu.Service.DTOs.Attachment;
+
+namespace SifatEdu.Service.Interfaces;
+
+public interface IAttachmentService
+{
+    Task<Attachment> UploadAsync(AttachmentCreationDto dto);
+    Task<bool> DeleteAsync(long id);
+}
