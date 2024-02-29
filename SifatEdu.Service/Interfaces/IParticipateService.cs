c35fa20378140a0731f308856fb1b685826bd09e
Ferganskiy <odilovmadiyor029@gmail.com>
2024-02-29 11:54:55 +0500

    Participate Service's interface added

---
+++ SifatEdu.Service/Interfaces/IParticipateService.cs
@@ -0,0 +1,15 @@
+﻿using SifatEdu.Service.DTOs.Participate;
+
+namespace SifatEdu.Service.Interfaces;
+
+public interface IParticipateService
+{
+    Task<bool> DeleteAsync(long id);
+    Task<IEnumerable<ParticipateResultDto>> GetAllAsync();
+    Task<ParticipateResultDto> GetByIdAsync(long id);
+    Task<IEnumerable<ParticipateResultDto>> GetByTestId(long testId);
+    Task<IEnumerable<ParticipateResultDto>> GetByUserId(long userId);
+    Task<float> MyScore(long userId, long examId);
+    Task<ParticipateResultDto> UpdateAsync(ParticipateUpdateDto updateDto);
+    Task<ParticipateResultDto> CreateAsync(ParticipateCreationDto creationDto);
+}
