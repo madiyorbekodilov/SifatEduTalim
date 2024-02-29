ba0c1b90c25642984a601be2d034de66a2922e2d
Ferganskiy <odilovmadiyor029@gmail.com>
2024-02-29 11:21:44 +0500

    Answer Service's interface added

---
+++ SifatEdu.Service/Interfaces/IAnswerService.cs
@@ -0,0 +1,14 @@
+﻿using SifatEdu.Domain.Entities;
+using SifatEdu.Service.DTOs.Answer;
+
+namespace SifatEdu.Service.Interfaces;
+
+public interface IAnswerService
+{
+    Task<bool> DeleteAsync(long id);
+    Task<IEnumerable<Answer>> GetAllAsync();
+    Task<AnswerResultDto> GetByIdAsync(long id);
+    Task<AnswerResultDto> ModifyAsync(AnswerUpdateDto answerUpdate);
+    Task<AnswerResultDto> CreateAsync(AnswerCreationDto answerCreation);
+    Task<IEnumerable<AnswerResultDto>> GetByQuestionIdAsync(long questionId);
+}
