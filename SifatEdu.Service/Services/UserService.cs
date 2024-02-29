321912295489d3222ed292dd1cd5286b57ec0b57
Ferganskiy <odilovmadiyor029@gmail.com>
2024-02-29 20:03:42 +0500

    UserService added

---
+++ SifatEdu.Service/Services/UserService.cs
@@ -0,0 +1,64 @@
+﻿using AutoMapper;
+using SifatEdu.Data.IRepasitories;
+using SifatEdu.Domain.Configurations;
+using SifatEdu.Domain.Entities;
+using SifatEdu.Domain.Enums;
+using SifatEdu.Service.DTOs.User;
+using SifatEdu.Service.Exceptions;
+using SifatEdu.Service.Helpers;
+using SifatEdu.Service.Interfaces;
+
+namespace SifatEdu.Service.Services;
+
+public class UserService : IUserService
+{
+    public Task<bool> CheckUserAsync(string emailOrUsername, string password)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<UserResultDto> CreateAsync(UserCreationDto dto)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> DeleteAsync(long id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<UserResultDto>> GetAllAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<UserResultDto> GetByEmailAsync(string email)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<UserResultDto> GetByIdAsync(long id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<UserResultDto>> GetByName(string name)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<IEnumerable<UserResultDto>> GetByUsernameAsync(string username)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<UserResultDto> ModifyAsync(UserUpdateDto dto)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<UserResultDto> ModifyPasswordAsync(long id, string oldPass, string newPass)
+    {
+        throw new NotImplementedException();
+    }
+}
