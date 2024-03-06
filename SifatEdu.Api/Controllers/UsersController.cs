8ea19cd79bf75504e32ff66c19e11a52c4d2f287
Ferganskiy <odilovmadiyor029@gmail.com>
2024-03-06 17:37:14 +0500

    UsersController added

---
+++ SifatEdu.Api/Controllers/UsersController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SifatEdu.Api.Models;
+using SifatEdu.Service.DTOs.User;
+using SifatEdu.Service.Helpers;
+using SifatEdu.Service.Interfaces;
+
+namespace SifatEdu.Api.Controllers;
+
+public class UsersController : BaseController
+{
+    private readonly IUserService userService;
+
+    public UsersController(IUserService userService)
+    {
+        this.userService = userService;
+    }
+
+    [HttpPost("register")]
+    public async Task<IActionResult> PostAsync(UserCreationDto dto)
+    {
+        var emailValid = Validator.IsValidEmail(dto.Email);
+        var passwordValid = Validator.IsValidPassword(dto.Password);
+        var nameValid = Validator.IsValidName(dto.FirsName);
+        var surnameValid = Validator.IsValidName(dto.LastName);
+
+        if (emailValid && passwordValid && nameValid && surnameValid)
+            return Ok(new Response
+            {
+                StatusCode = 200,
+                Message = "Success",
+                Data = await this.userService.AddAsync(dto)
+            });
+        return BadRequest("Invalid Information");
+    }
+
+    [HttpPut("update")]
+    public async Task<IActionResult> PutAsync(UserUpdateDto dto)
+    {
+        var emailValid = Validator.IsValidEmail(dto.Phone);
+        var nameValid = Validator.IsValidName(dto.FirsName);
+        var surnameValid = Validator.IsValidName(dto.LastName);
+        
+        if(emailValid && nameValid && surnameValid)
+            return Ok(new Response
+            {
+                StatusCode = 200,
+                Message = "Success",
+                Data = await this.userService.ModifyAsync(dto)
+            });
+
+        return BadRequest("Invalid Information");
+    }
+
+    [HttpDelete("get/{id/long}")]
+    public async Task<IActionResult> DeleteAsync(long id)
+        => Ok(new Response
+        {
+            StatusCode = 200,
+            Message = "Success",
+            Data = await this.userService.RemoveAsync(id)
+        });
+
+}
--- SifatEdu.Service/DTOs/User/UserUpdateDto.cs
+++ SifatEdu.Service/DTOs/User/UserUpdateDto.cs
@@ -7,8 +7,6 @@ public class UserUpdateDto
     public long Id { get; set; }
     public string FirsName { get; set; }
     public string LastName { get; set; }
-    public string Email { get; set; }
-    public string Password { get; set; }
     public string Phone { get; set; }
     public UserRole Role { get; set; }
     public DateTime DateOfBirth { get; set; }
