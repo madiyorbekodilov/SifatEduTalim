ce4b6b387d3cf1740c55a6dcbc8771c7cf3e8d81
Ferganskiy <odilovmadiyor029@gmail.com>
2024-03-06 14:18:05 +0500

    CodeUchunService added

--- SifatEdu.Service/Services/CodeUchunService.cs
+++ SifatEdu.Service/Services/CodeUchunService.cs
@@ -1,8 +1,9 @@
 ﻿using AutoMapper;
-using SifatEdu.Data.IRepasitories;
 using SifatEdu.Domain.Entities;
-using SifatEdu.Service.DTOs.CodeUchun;
+using SifatEdu.Service.Exceptions;
 using SifatEdu.Service.Interfaces;
+using SifatEdu.Data.IRepasitories;
+using SifatEdu.Service.DTOs.CodeUchun;
 
 namespace SifatEdu.Service.Services;
 
@@ -10,40 +11,86 @@ public class CodeUchunService : ICodeuchunService
 {
     private readonly IMapper mapper;
     private readonly IRepasitory<CodeUchun> repasitory;
+    private readonly IQuestionService questionService;
 
-    public CodeUchunService(IRepasitory<CodeUchun> repasitory, IMapper mapper)
+    public CodeUchunService(IRepasitory<CodeUchun> repasitory, IQuestionService questionService, IMapper mapper)
     {
         this.mapper = mapper;
         this.repasitory = repasitory;
+        this.questionService = questionService;
     }
 
-    public Task<CodeUchunResultDto> CreateAsync(CodeUchunCreationDto CodeCreation)
+    public async Task<CodeUchunResultDto> CreateAsync(CodeUchunCreationDto CodeCreation)
     {
-        throw new NotImplementedException();
+        var existQuestion = await this.questionService.GetByIdAsync(CodeCreation.QuestionId);
+
+        if (existQuestion is null)
+            throw new NotFoundException("Question not found");
+
+        var answer = this.mapper.Map<CodeUchun>(CodeCreation);
+
+        await this.repasitory.CreateAsync(answer);
+        await this.repasitory.SaveAsync();
+
+        return this.mapper.Map<CodeUchunResultDto>(answer);
     }
 
-    public Task<bool> DeleteAsync(long id)
+    public async Task<bool> DeleteAsync(long id)
     {
-        throw new NotImplementedException();
+        var question = await repasitory.SelectAsync(x => x.Id == id);
+
+        if (question is null)
+            throw new NotFoundException("Not found");
+
+        this.repasitory.Delete(question);
+        await this.repasitory.SaveAsync();
+
+        return true;
     }
 
-    public Task<IEnumerable<CodeUchun>> GetAllAsync()
+    public async Task<IEnumerable<CodeUchun>> GetAllAsync()
     {
-        throw new NotImplementedException();
+        return this.repasitory.SelectAll(null, true, new string[] { "Question" });
     }
 
-    public Task<CodeUchunResultDto> GetByIdAsync(long id)
+    public async Task<CodeUchunResultDto> GetByIdAsync(long id)
     {
-        throw new NotImplementedException();
+        var dats = await this.repasitory.SelectAsync(x => x.Id == id);
+
+        if (dats is null)
+            throw new NotFoundException("Malumot topilmadi");
+
+        return this.mapper.Map<CodeUchunResultDto>(dats);
     }
 
-    public Task<IEnumerable<CodeUchunResultDto>> GetByQuestionIdAsync(long questionId)
+    public async Task<IEnumerable<CodeUchunResultDto>> GetByQuestionIdAsync(long questionId)
     {
-        throw new NotImplementedException();
+        var questionc = await this.questionService.GetByIdAsync(questionId);
+
+        if (questionc is null)
+            throw new NotFoundException("Not found");
+
+        var answers = this.repasitory.SelectAll(a => a.QuestionId == questionId);
+
+        return this.mapper.Map<IEnumerable<CodeUchunResultDto>>(answers);
     }
 
-    public Task<CodeUchunResultDto> ModifyAsync(CodeUchunUpdateDto CodeUpdate)
+    public async Task<CodeUchunResultDto> ModifyAsync(CodeUchunUpdateDto CodeUpdate)
     {
-        throw new NotImplementedException();
+        var exisAnswer = await this.repasitory.SelectAsync(x => x.Id == CodeUpdate.Id);
+        var exisQuestion = await this.questionService.GetByIdAsync(CodeUpdate.QuestionId);
+
+        if (exisAnswer is null)
+            throw new NotFoundException("Answer not found");
+
+        if (exisQuestion is null)
+            throw new NotFoundException("Question not found");
+
+        this.mapper.Map(CodeUpdate, exisAnswer);
+
+        this.repasitory.Update(exisAnswer);
+        await this.repasitory.SaveAsync();
+
+        return this.mapper.Map<CodeUchunResultDto>(exisAnswer);
     }
 }
