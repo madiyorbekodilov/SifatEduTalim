f30324cbb976f106071fa2bd33f13a7605e6bf62
Ferganskiy <odilovmadiyor029@gmail.com>
2024-03-06 17:00:06 +0500

    Controller added

---
+++ SifatEdu.Api/Controllers/AuthController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SifatEdu.Api.Models;
+using SifatEdu.Service.Helpers;
+using SifatEdu.Service.Interfaces;
+using System.Globalization;
+
+namespace SifatEdu.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AuthController : ControllerBase
+{
+    private readonly IAuthService authService;
+
+    public AuthController(IAuthService authService)
+    {
+        this.authService = authService;
+    }
+
+    [HttpPost("authenticate")]
+    [AllowAnonymous]
+    public async Task<IActionResult> AuthenticateAsync(string emailOrUsername, string password)
+    {
+        if(Validator.IsValidEmail(emailOrUsername)) 
+        {
+            return Ok(new Response
+            {
+                StatusCode = 200,
+                Message = "Succes",
+                Data = await this.authService.GenerateTokenAsync(emailOrUsername, password)
+            });
+        }
+        else if(Validator.IsValidPassword(password))
+        {
+            return Ok(new Response
+            {
+                StatusCode = 200,
+                Message = "Success",
+                Data = await this.authService.GenerateTokenAsync(emailOrUsername, password)
+            });
+        }
+            
+        return BadRequest("Username or password is incorrect");
+    }
+}
---
+++ SifatEdu.Api/Controllers/BaseController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SifatEdu.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class BaseController : ControllerBase
+{
+}
