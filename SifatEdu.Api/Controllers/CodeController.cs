4b1d0dc1029ffb868ef909c38bf1970e08a46c80
Ferganskiy <odilovmadiyor029@gmail.com>
2024-03-07 12:33:47 +0500

    CodeUchun's Controller added

---
+++ SifatEdu.Api/Controllers/CodeController.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SifatEdu.Api.Models;
+using SifatEdu.Service.DTOs.CodeUchun;
+using SifatEdu.Service.Helpers;
+using SifatEdu.Service.Interfaces;
+
+namespace SifatEdu.Api.Controllers;
+
+public class CodeController : BaseController
+{
+    private readonly ICodeuchunService service;
+
+    public CodeController(ICodeuchunService service)
+    {
+        this.service = service;
+    }
+
+    [HttpPost("create")]
+    public async Task<IActionResult> PostAsync(CodeUchunCreationDto dto)
+        => Ok(new Response
+        {
+            StatusCode = 200,
+            Message = "Success",
+            Data = await this.service.CreateAsync(dto)
+        });
+
+    [HttpPut("update")]
+    public async Task<IActionResult> PutAsync(CodeUchunUpdateDto dto)
+        => Ok(new Response
+        {
+            StatusCode = 200,
+            Message = "Success",
+            Data = await this.service.ModifyAsync(dto)
+        });
+
+    [HttpDelete("delete/{id:long}")]
+    public async Task<IActionResult> DeleteAsync(long id)
+        => Ok(new Response
+        {
+            StatusCode = 200,
+            Message = "Success",
+            Data = await this.service.DeleteAsync(id)
+        });
+
+    [HttpGet("get/{id:long}")]
+    public async Task<IActionResult> GetByIdAsync(long id)
+        => Ok(new Response
+        {
+            StatusCode = 200,
+            Message = "Success",
+            Data = await this.service.GetByIdAsync(id)
+        });
+
+    [HttpGet("get-all")]
+    public async Task<IActionResult> GetAllAsync()
+        => Ok(new Response
+        {
+            StatusCode = 200,
+            Message = "Success",
+            Data = await this.service.GetAllAsync()
+        });
+
+    [HttpGet("get")]
+    public async Task<IActionResult> GetByQuestionIdAsync(long questionId)
+        => Ok(new Response
+        {
+            StatusCode = 200,
+            Message = "Success",
+            Data = await this.service.GetByQuestionIdAsync(questionId)
+        });
+}
